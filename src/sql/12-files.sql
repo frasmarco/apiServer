42339a426d53e67cacf1f3257dc84cdbf1f24c8d
frasmarco <fras.marco@gmail.com>
2017-09-18 17:48:44 +0200

    Create 12-files.sql

---
+++ src/sql/12-files.sql
@@ -0,0 +1,20 @@
+
+CREATE TABLE public.files (
+  md5 UUID,
+  mime_type VARCHAR(30) NOT NULL,
+  filename INTEGER NOT NULL,
+  created_at TIMESTAMP WITH TIME ZONE DEFAULT timezone('utc'::text, now()) NOT NULL,
+  created_by UUID NOT NULL REFERENCES public.user_profile(user_id),
+  deleted BOOLEAN DEFAULT false,
+  PRIMARY KEY(md5)
+) 
+WITH (oids = false);
+
+COMMENT ON COLUMN public.files.md5
+IS 'MD5 hash of file contents';
+
+COMMENT ON COLUMN public.files.mime_type
+IS 'Mime Type derived from actual file contents';
+
+COMMENT ON COLUMN public.files.filename
+IS 'Original filename';
